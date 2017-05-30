dd35e564212be87f7738029f7e72a3e60e3f7778
slowbot <jesse@slowbot.com>
2017-05-30 13:03:29 -0700

    EARTH-6: Reformating source.s file based on Drupal documentation

--- js/source/scripts.js
+++ js/source/scripts.js
@@ -11,113 +11,113 @@
 // - https://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 
-(function ($) {
+(function ($, Drupal) {
 
   // To understand behaviors, see https://www.drupal.org/node/2269515
-  Drupal.behaviors.basic = {
+  Drupal.behaviors.matson = {
     attach: function (context, settings) {
-
-      // Execute code once the DOM is ready. $(document).ready() not required within Drupal.behaviors.
-      $(window).load(function () {
-        // Execute code once the window is fully loaded.
-
-        var ww = document.body.clientWidth;
-        var toggleWidth = 768;
-
-        // Initial dropdown setup.
-        addParents();
-        adjustMenu(ww, toggleWidth);
-
-        $(window).bind('resize orientationchange', function() {
-          ww = document.body.clientWidth;
-          adjustMenu(ww, 768);
-        });
-
-        $(".js-nav-mobile-closable").on('click', function(e) {
-          e.preventDefault();
-          var $this = $(this)
-
-          if ($this.hasClass('is-active')) {
-            $this.trigger('navbar:close')
-          }
-          else {
-            $this.trigger('navbar:open')
-          }
-        });
-
-        $(document).on('navbar:open', function(e) {
-          var $navbar = $(".js-nav-expandable");
-          var $navbarButton = $(".js-nav-mobile-closable");
-
-          $navbarButton.addClass('is-active');
-          $navbarButton.next(".js-nav-group-viewable").show();
-          $navbar.css({'min-height': ($(".hero-banner").height() + 'px')});
-          $navbar.addClass('is-expanded');
-          $(".hero-banner__header").hide();
-          adjustMenu();
-        })
-
-        $(document).on('navbar:close', function(e) {
-          var $navbar = $(".js-nav-expandable")
-          var $navbarButton = $(".js-nav-mobile-closable");
-
-          $navbarButton.removeClass('is-active');
-          $navbar.css({'min-height': '65px'});
-          $navbar.removeClass('is-expanded');
-          if (!e.navbarExpanded) {
-            $navbarButton.next(".js-nav-group-viewable").hide();
-          }
-          $(".hero-banner__header").show();
-        })
-
-        function addParents() {
-          $(".js-nav-item-expandable li a").each(function() { // basic-main-menu.
-            if ($(this).next().length > 0) {
-              $(this).addClass('parent');
-            }
-          });
-        }
-
-        function adjustMenu(ww, toggleWidth) {
-          $(".js-nav-item-expandable li").unbind('click'); // basic-main-menu
-          $(".js-nav-item-expandable li a.parent").unbind('click').bind('click', function(e) { // basic-main-menu.
-            // Must be attached to anchor element to prevent bubbling.
-            event.stopPropagation();
-            e.preventDefault();
-            $(this).parent("li").toggleClass('is-open').siblings("li").removeClass('is-open');
-          });
-
-          if (ww < toggleWidth) {
-            if (!$(".js-nav-mobile-closable").hasClass('is-active')) {
-              $(".js-nav-group-viewable").hide();
-            }
-            else {
-              $(".js-nav-group-viewable").show();
-            }
-          }
-          else if (ww >= toggleWidth) {
-            $(".js-nav-group-viewable").show();
-            $(".js-nav-item-expandable li").unbind('click'); // basic-main-menu.
-
-            $(document).click(function() {
-              $(".js-nav-item-expandable li").removeClass('is-open'); // basic-main-menu.
-            });
-          }
-        }
-      });
-
-      $(window).resize(Drupal.debounce(function () {
-        // Execute code when the window is resized.
-        var vw = $(window).width();
-        if (vw > 768) {
-          $(document).trigger({ type: 'navbar:close', navbarExpanded: true })
-        }
-      }, 400));
-
       $(window).scroll(function () {
         // Execute code when the window scrolls.
       });
     }
   };
 
-} (jQuery));
+  // Execute code once the DOM is ready.
+  $(document).ready(function () {
+    $(window).load(function () {
+      // Execute code once the window is fully loaded.
+
+      var ww = document.body.clientWidth;
+      var toggleWidth = 768;
+
+      // Initial dropdown setup.
+      addParents();
+      adjustMenu(ww, toggleWidth);
+
+      $(window).bind('resize orientationchange', function() {
+        ww = document.body.clientWidth;
+        adjustMenu(ww, 768);
+      });
+
+      $(".js-nav-mobile-closable").on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this)
+
+        if ($this.hasClass('is-active')) {
+          $this.trigger('navbar:close')
+        }
+        else {
+          $this.trigger('navbar:open')
+        }
+      });
+
+      $(document).on('navbar:open', function(e) {
+        var $navbar = $(".js-nav-expandable");
+        var $navbarButton = $(".js-nav-mobile-closable");
+
+        $navbarButton.addClass('is-active');
+        $navbarButton.next(".js-nav-group-viewable").show();
+        $navbar.css({'min-height': ($(".hero-banner").height() + 'px')});
+        $navbar.addClass('is-expanded');
+        $(".hero-banner__header").hide();
+        adjustMenu();
+      })
+
+      $(document).on('navbar:close', function(e) {
+        var $navbar = $(".js-nav-expandable")
+        var $navbarButton = $(".js-nav-mobile-closable");
+
+        $navbarButton.removeClass('is-active');
+        $navbar.css({'min-height': '65px'});
+        $navbar.removeClass('is-expanded');
+        if (!e.navbarExpanded) {
+          $navbarButton.next(".js-nav-group-viewable").hide();
+        }
+        $(".hero-banner__header").show();
+      })
+
+      function addParents() {
+        $(".js-nav-item-expandable li a").each(function() { // basic-main-menu.
+          if ($(this).next().length > 0) {
+            $(this).addClass('parent');
+          }
+        });
+      }
+
+      function adjustMenu(ww, toggleWidth) {
+        $(".js-nav-item-expandable li").unbind('click'); // basic-main-menu
+        $(".js-nav-item-expandable li a.parent").unbind('click').bind('click', function(e) { // basic-main-menu.
+          // Must be attached to anchor element to prevent bubbling.
+          event.stopPropagation();
+          e.preventDefault();
+          $(this).parent("li").toggleClass('is-open').siblings("li").removeClass('is-open');
+        });
+
+        if (ww < toggleWidth) {
+          if (!$(".js-nav-mobile-closable").hasClass('is-active')) {
+            $(".js-nav-group-viewable").hide();
+          }
+          else {
+            $(".js-nav-group-viewable").show();
+          }
+        }
+        else if (ww >= toggleWidth) {
+          $(".js-nav-group-viewable").show();
+          $(".js-nav-item-expandable li").unbind('click'); // basic-main-menu.
+
+          $(document).click(function() {
+            $(".js-nav-item-expandable li").removeClass('is-open'); // basic-main-menu.
+          });
+        }
+      }
+    });
+
+    $(window).resize(Drupal.debounce(function () {
+      // Execute code when the window is resized.
+      var vw = $(window).width();
+      if (vw > 768) {
+        $(document).trigger({ type: 'navbar:close', navbarExpanded: true })
+      }
+    }, 400));
+  });
+} (jQuery, Drupal));
