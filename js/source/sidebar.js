de14d0dca3818d1b2b0b3986817eff7879515a46
Jordan <jordan@koplowicz.com>
2017-06-24 13:25:35 -0700

    EARTH-145: (a) Added compiled css & js, (b) completed responsive design, (c) cross-browser compatibility complete

--- js/source/sidebar.js
+++ js/source/sidebar.js
@@ -19,41 +19,44 @@
       // Execute code once the DOM is ready. $(document).ready() not required within Drupal.behaviors.
 
       $(window).load(function () {
-        // Check banner height.  If there is no banner, this returns null.
-        var bannerHeight =  $('div.hero-banner').height();
-        // Check if there's a sidebar (eg. Department pages)
-        var classy = $('body').hasClass('with-sidebar');
+        // If it's a mobile device, don't do anything.
+        if ( $(window).width() > 860 ) {
+          // Check banner height.  If there is no banner, this returns null.
+          var bannerHeight =  $('div.hero-banner').height();
+          // Check if there's a sidebar (eg. Department pages)
+          var classy = $('body').hasClass('with-sidebar');
+          // Check if there's both a sidebar and a banner.
+          // If there is, then transform the header lockup, add some height, and
+          // add the .with-banner class to body, so the css can apply more styling
+          if (bannerHeight && classy) {
+            $('#block-matson-branding').css({'height': bannerHeight});
+            $('body').addClass('with-banner');
+          }
 
-        // Check if there's both a sidebar and a banner.
-        // If there is, then transform the header lockup, add some height, and
-        // add the .with-banner class to body, so the css can apply more styling
-        if (bannerHeight && classy) {
-          $('#block-matson-branding').css({'min-height': bannerHeight});
-          $('body').addClass('with-banner');
+          // Gather a bunch of measurements to create offset for sidebar
+          var brandHeight = $('.brand-bar').height();
+          var toolbarHeight = $('#toolbar-bar').height();
+          var toolbarTopHeight = $('#toolbar-item-administration-tray').height();
+          var headerHeight = $('#header').height();
+          var contentHeight = $('div.main-container').height();
+          var otherHeight = bannerHeight ? -2 : 26; // I don't know why this works
+          var sidebar__offset = brandHeight + toolbarHeight + toolbarTopHeight + bannerHeight + headerHeight + otherHeight;
+          // console.log(bannerHeight, contentHeight, sidebar__offset, otherHeight);
+          // $('#sidebar__wrapper').css({'top': '780px'});
+          $('#sidebar__wrapper').css({'top': sidebar__offset, 'height': contentHeight - bannerHeight});
+
+
+          // Add ScrollToFixed functionality to sidebar region
+          var topLimit = $('body').hasClass('toolbar-fixed') ? 80 : 0;
+          var bottomLimit = $('#footer__container').offset().top - $('#sidebar').outerHeight(true);
+          // console.log(topLimit, bottomLimit);
+          $("#sidebar").scrollToFixed( {
+            marginTop: topLimit,
+            limit: bottomLimit,
+            removeOffsets: true
+          } );
+
         }
-
-        // Gather a bunch of measurements to create offset for sidebar
-        var brandHeight = $('.brand-bar').height();
-        var toolbarHeight = $('#toolbar-bar').height();
-        var toolbarTopHeight = $('#toolbar-item-administration-tray').height();
-        var headerHeight = $('#header').height();
-        var contentHeight = $('div.main-container').height();
-        var otherHeight = bannerHeight ? -2 : 26; // I don't know why this works
-        var sidebar__offset = brandHeight + toolbarHeight + toolbarTopHeight + bannerHeight + headerHeight + otherHeight;
-        // console.log(bannerHeight, contentHeight, sidebar__offset, otherHeight);
-        // $('#sidebar__wrapper').css({'top': '780px'});
-        $('#sidebar__wrapper').css({'top': sidebar__offset, 'height': contentHeight - bannerHeight});
-
-
-        // Add ScrollToFixed functionality to sidebar region
-        var topLimit = $('body').hasClass('toolbar-fixed') ? 80 : 0;
-        var bottomLimit = $('#footer__container').offset().top - $('#sidebar').outerHeight(true);
-        // console.log(topLimit, bottomLimit);
-        $("#sidebar").scrollToFixed( {
-          marginTop: topLimit,
-          limit: bottomLimit,
-          removeOffsets: true
-        } );
 
       });
 
