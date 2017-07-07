9a290df4a10c50ff1121123187aab5185c8cfc5a
Jordan <jordan@koplowicz.com>
2017-07-07 12:07:33 -0700

    EARTH-248: working to refactor sidebar functionality to work more closely with stanford_layout

--- js/source/sidebar.js
+++ js/source/sidebar.js
@@ -12,20 +12,19 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function (Drupal, $, window) {
 
-  // To understand behaviors, see https://www.drupal.org/node/2269515
-  Drupal.behaviors.basic = {
-    attach: function (context, settings) {
+      function mylifeforaiur () {
+        // If you're using the "aiur" layout from stanford_layouts, then you get
+        // a floating sidebar.
+        // Only do this if you're not on a mobile device.
 
-      // Execute code once the DOM is ready. $(document).ready() not required within Drupal.behaviors.
+        console.log("OMG I'M LOADING A THING");
 
-      $(window).load(function () {
-        // If it's a mobile device, don't do anything.
         if ( $(window).width() > 860 ) {
           // Check banner height.  If there is no banner, this returns null.
-          var bannerHeight =  $('div.hero-banner').height();
+          var bannerHeight =  $('div.hero-banner').first().height();
           // Check if there's a sidebar (eg. Department pages)
-          var classy = $('section').hasClass('with-sidebar');
-          console.log(classy);
+          var classy = $('section').hasClass('with-floating_sidebar');
+
           // Check if there's both a sidebar and a banner.
           // If there is, then transform the header lockup, add some height, and
           // add the .with-banner class to body, so the css can apply more styling
@@ -40,37 +39,42 @@
           var toolbarTopHeight = $('#toolbar-item-administration-tray').height();
           var headerHeight = $('#header').height();
           var contentHeight = $('div.main-container').height();
-          // var otherHeight = bannerHeight ? -2 : 26; // I don't know why this works
           var otherHeight = 26; // I don't know why this works
-          var sidebar__offset = brandHeight + toolbarHeight + toolbarTopHeight + bannerHeight + headerHeight + otherHeight;
-          // console.log(bannerHeight, contentHeight, sidebar__offset, otherHeight);
-          // $('#sidebar__wrapper').css({'top': '780px'});
-          $('#sidebar__wrapper').css({'top': sidebar__offset, 'height': contentHeight - bannerHeight});
-
+          var floating_sidebar__offset = brandHeight + toolbarHeight + toolbarTopHeight + bannerHeight + headerHeight + otherHeight;
+          $('#floating_sidebar__wrapper').css({'top': floating_sidebar__offset, 'height': contentHeight - bannerHeight});
 
           // Add ScrollToFixed functionality to sidebar region
-          var topLimit = $('body').hasClass('toolbar-fixed') ? 80 : 0;
-          var bottomLimit = $('#footer__container').offset().top - $('#sidebar').outerHeight(true);
-          // console.log(topLimit, bottomLimit);
-          $("#sidebar").scrollToFixed( {
+          var topLimit = $('body').hasClass('toolbar-fixed') ? 100 : 19;
+          var bottomLimit = $('#footer__container').offset().top - $('#floating_sidebar').outerHeight(true);
+          $('#floating_sidebar').scrollToFixed( {
             marginTop: topLimit,
             limit: bottomLimit,
             removeOffsets: true
           } );
-
+        } else {
+          // For true fluid responsiveness, if you resize the window from large
+          // to small, then it should remove all the styling and stuff.
+          $('body').removeClass('with-banner');
+          $('#block-matson-branding').removeAttr( 'style' );
+          $('#floating_sidebar__wrapper').removeAttr( 'style' );
+          $('#floating_sidebar').trigger('detach.ScrollToFixed');
         }
 
+      }
+
+      // Execute code once the DOM is ready. $(document).ready() not required within Drupal.behaviors.
+
+      $(window).load(function () {
+        mylifeforaiur();
       });
 
       $(window).resize(function () {
         // Execute code when the window is resized.
+        mylifeforaiur();
       });
 
       $(window).scroll(function () {
         // Execute code when the window scrolls.
       });
-
-    }
-  };
 
 } (Drupal, jQuery, this));
