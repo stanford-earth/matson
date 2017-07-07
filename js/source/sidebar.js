61d27efb39b9dc9b8c13cda9341dbfd9b24aa71e
Jordan <jordan@koplowicz.com>
2017-07-07 15:11:19 -0700

    EARTH-248: conversation w. Shea in progress

--- js/source/sidebar.js
+++ js/source/sidebar.js
@@ -17,34 +17,36 @@
         // a floating sidebar.
         // Only do this if you're not on a mobile device.
 
-        console.log("OMG I'M LOADING A THING");
-
         if ( $(window).width() > 860 ) {
           // Check banner height.  If there is no banner, this returns null.
           var bannerHeight =  $('div.hero-banner').first().height();
-          // Check if there's a sidebar (eg. Department pages)
-          var classy = $('section').hasClass('with-floating_sidebar');
 
           // Check if there's both a sidebar and a banner.
           // If there is, then transform the header lockup, add some height, and
           // add the .with-banner class to body, so the css can apply more styling
-          if (bannerHeight && classy) {
+          if (bannerHeight) {
             $('#block-matson-branding').css({'height': bannerHeight});
             $('body').addClass('with-banner');
           }
 
           // Gather a bunch of measurements to create offset for sidebar
           var brandHeight = $('.brand-bar').height();
-          var toolbarHeight = $('#toolbar-bar').height();
-          var toolbarTopHeight = $('#toolbar-item-administration-tray').height();
           var headerHeight = $('#header').height();
           var contentHeight = $('div.main-container').height();
-          var otherHeight = 26; // I don't know why this works
-          var floating_sidebar__offset = brandHeight + toolbarHeight + toolbarTopHeight + bannerHeight + headerHeight + otherHeight;
+
+          // This works a little differently if you're logged in because of the toolbar
+          var topLimit, floating_sidebar__offset;
+          if ($('body').hasClass('toolbar-fixed')) {
+            topLimit = 100;
+            floating_sidebar__offset = bannerHeight + headerHeight;
+          } else {
+            topLimit = 19;
+            floating_sidebar__offset = bannerHeight + headerHeight + brandHeight + 26;
+          }
+
           $('#floating_sidebar__wrapper').css({'top': floating_sidebar__offset, 'height': contentHeight - bannerHeight});
 
           // Add ScrollToFixed functionality to sidebar region
-          var topLimit = $('body').hasClass('toolbar-fixed') ? 100 : 19;
           var bottomLimit = $('#footer__container').offset().top - $('#floating_sidebar').outerHeight(true);
           $('#floating_sidebar').scrollToFixed( {
             marginTop: topLimit,
