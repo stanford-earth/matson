2d946c849bbed2b7de5a158281bdb13c5b54fa20
Shea McKinney <sherakama@gmail.com>
2018-03-06 15:20:26 -0800

    fixup! wip

--- js/source/sharre.init.js
+++ js/source/sharre.init.js
@@ -2,6 +2,13 @@
   Drupal.behaviors.matson_sharrre = {
     attach: function (context, settings) {
 
+      // Get the current url for twitter
+      var newurl = document.location.href;
+      // Strip paramaters
+      newurl = newurl.replace(window.location.search, "");
+      // Force protocol to http.
+      newurl = newurl.replace(/.*?:\/\//g, "http://");
+
       $('.tweet-popup').sharrre({
         share: {
           twitter: true
@@ -10,9 +17,9 @@
         enableCounter: true,
         enableHover: false,
         enableTracking: true,
-        buttons: { twitter: { via: 'StanfordEarth', url: '//opensharecount.com/count.json?url=' + document.location.href, count: true } },
+        buttons: { twitter: { via: 'StanfordEarth', url: '//opensharecount.com/count.json?url=' + newurl, count: true } },
         click: function(api, options) {
-          api.simulateClick();
+          api.simulateClick(".tweet-popup");
           api.openPopup('twitter');
         }
       });
@@ -25,7 +32,7 @@
         enableTracking: true,
         buttons: { facebook: {action: 'share', title: 'Share'}},
         click: function(api, options) {
-          api.simulateClick();
+          api.simulateClick('.facebook-popup');
           api.openPopup('facebook');
         }
       });
@@ -38,7 +45,7 @@
         enableTracking: true,
         buttons: { linkedin: {action: 'share', title: 'Share'}},
         click: function(api, options) {
-          api.simulateClick();
+          api.simulateClick('.linkedin-popup');
           api.openPopup('linkedin');
         }
       });
