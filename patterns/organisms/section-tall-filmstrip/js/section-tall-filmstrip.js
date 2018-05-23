c9c313188d2429dd95dba30a21a27f31095f9590
Sherakama <sherakama@gmail.com>
2018-05-23 12:58:55 -0700

    EARTH-286: Voiceover fixes for tall filmstrip (#148)
    * EARTH-286: Accessibility fixes for tall-filmstrip.
    
    * EARTH-286: Accessibility fixes for tall-filmstrip.

--- patterns/organisms/section-tall-filmstrip/js/section-tall-filmstrip.js
+++ patterns/organisms/section-tall-filmstrip/js/section-tall-filmstrip.js
@@ -4,7 +4,25 @@
   $(window).bind('load', function () {
     $(".js-expandable-container__toggle").unbind('click').bind('click', function(e) { // expandable card
       $(this).parent(".js-expandable-container").addClass('is-open');
-      $(this).parent(".js-expandable-container").find(".film-card__link").eq(3).focus();
+      $(this).parent(".js-expandable-container")
+        .parent()
+        .find(".aria-alert")
+        .toggleClass('hidden')
+        .attr("role", "alert")
+        .fadeOut(100);
+
+        $(this).parent(".js-expandable-container")
+          .parent()
+          .find(".aria-alert")
+          .promise()
+          .done(function() {
+            $(this).parents(".js-expandable-container")
+              .find(".film-card__link")
+              .eq(4)
+              .focus()
+              .attr("role", "alert");
+          });
+
       // Must be attached to anchor element to prevent bubbling.
       e.preventDefault();
       e.stopPropagation();
