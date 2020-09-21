d38b69a1699a73fb5a7ebaa452902caa34fb518e
Katria Lesser <katrialesser@users.noreply.github.com>
2020-09-21 10:26:42 -0600

    EARTH-1450: Wrapped search js in on window load function. (#215)
    * EARTH-1450: Wrapped search js in on window load function.
    
    * EARTH-1450: search js on load function compiled
    
    Co-authored-by: Katria Lesser <Katria@stanford.edu>
    Co-authored-by: Dena DeBry <dena@buttonwillowsix.com>

--- js/source/search.js
+++ js/source/search.js
@@ -1,23 +1,25 @@
-(function($) {
-  if ($('#header #views-exposed-form-acquia-search-page input[data-drupal-selector="edit-search"]')) {
-    $('#header #views-exposed-form-acquia-search-page input[data-drupal-selector="edit-search"]').focus(function(){
-      if (document.getElementById('edit-search') !== null) {
-        document.getElementById('edit-search').value = '';
-      } else if (document.getElementById('edit-search--2' !== null)) {
-        document.getElementById('edit-search--2').value = '';
-      }
-      $('#header #views-exposed-form-acquia-search-page').addClass('expanded');
-      $('#header .navigation__main-menu .basic-main-menu').fadeOut(500);
+window.onload = function() {
+  (function ($) {
+    if ($('#header #views-exposed-form-acquia-search-page input[data-drupal-selector="edit-search"]')) {
+      $('#header #views-exposed-form-acquia-search-page input[data-drupal-selector="edit-search"]').focus(function () {
+        if (document.getElementById('edit-search') !== null) {
+          document.getElementById('edit-search').value = '';
+        } else if (document.getElementById('edit-search--2' !== null)) {
+          document.getElementById('edit-search--2').value = '';
+        }
+        $('#header #views-exposed-form-acquia-search-page').addClass('expanded');
+        $('#header .navigation__main-menu .basic-main-menu').fadeOut(500);
+      });
+      $('#header #views-exposed-form-acquia-search-page input[data-drupal-selector="edit-search"]').focusout(function () {
+        $('#header #views-exposed-form-acquia-search-page').removeClass('expanded');
+        $('#header .navigation__main-menu .basic-main-menu').fadeIn(1000);
+      });
+    }
+    $('#header #views-exposed-form-acquia-search-page #edit-submit-acquia-search').click(function () {
+      $('#header #views-exposed-form-acquia-search-page').submit();
     });
-    $('#header #views-exposed-form-acquia-search-page input[data-drupal-selector="edit-search"]').focusout(function(){
-      $('#header #views-exposed-form-acquia-search-page').removeClass('expanded');
-      $('#header .navigation__main-menu .basic-main-menu').fadeIn(1000);
-    });
-  }
-  $('#header #views-exposed-form-acquia-search-page #edit-submit-acquia-search').click(function(){
-    $('#header #views-exposed-form-acquia-search-page').submit();
-  });
-  if ($('body').hasClass('page-search')) {
-    $('.main-container input[data-drupal-selector="edit-search"]').attr('placeholder', 'Search Stanford Earth');
-  }
-})(jQuery);
+    if ($('body').hasClass('page-search')) {
+      $('.main-container input[data-drupal-selector="edit-search"]').attr('placeholder', 'Search Stanford Earth');
+    }
+  })(jQuery);
+};
